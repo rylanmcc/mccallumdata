7b6362e2c5fd17b8801ea197b6a6cb9332b4a5ce
rylan <rylan_mccallum@hotmail.com>
2019-10-03 11:25:39 -0700

    tried to manipulate data but did not work - ignore this push

--- 2_SeedPredationDataExploration.R
+++ 2_SeedPredationDataExploration.R
@@ -6,13 +6,7 @@ library("tidyverse")
 Dat <- read_csv("SeedPredationDataForR2019.csv") %>% 
   mutate(N_start = if_else(Species=="sun", 5, ifelse(Species=="oats", 8, NA)),
          Prop_rmv = 1-N_end/N_start)
-#test for normality of data
-#run linear models
-#interactions?
-#want temp and elevation as well
-lm.1<-lm(Prop_rmv~Species+Canopy,data=Dat)
-summary(lm.1)
-anova(lm.1)
+
 
 ### take a look at distributions of removal rates
 library(ggplot2)
@@ -35,6 +29,39 @@ mean(sun$Prop_rmv)
 ### mean of oats removed
 oats <- Dat[which(Dat$Species == "oats"),]
 mean(oats$Prop_rmv)
+
+#test for normality of data
+#run linear models
+#interactions?
+#want temp and elevation as well
+lm.1<-lm(Prop_rmv~Species+Canopy,data=Dat)
+summary(lm.1)
+anova(lm.1)
+
+### Trying to figure out how to link canopy to site names
+### Link "closed" to **RP-3-1** and "open" to **RP-3-2** 
+
+summary(canopy)
+
+canopy <- Dat$Canopy
+summary(canopy)
+
+### saved the elevation, latitude, and longitude as an easier name to work with 
+
+LatLongElv_2017 <- SeedAdditionTransectGPSPoints2017
+
+### Making a new data frame to test if i can merge my two existing data frames together 
+
+TestMergeData <- Dat
+
+MergedDataFrame <- merge(TestMergeData, LatLongElv_2017)
+
+cbind(LatLongElv_2017$`Site Name`, TestMergeData)
+
+TestMergeData$GivenSiteName <- ifelse((TestMergeData$Site = RP-1B) & (TestMergeData$Canopy = open), LatLongElv_2017$`Site Name`
+
+
+
 
 
 
