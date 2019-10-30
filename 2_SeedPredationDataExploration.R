2c3282066992952db4884b7c93cbb22a80ff61bb
rylan <rylan_mccallum@hotmail.com>
2019-10-30 16:24:02 -0700

    scaled elevation to allow mod2 to run properly

--- 2_SeedPredationDataExploration.R
+++ 2_SeedPredationDataExploration.R
@@ -36,7 +36,7 @@ plot(Prop_rmv ~ Date2, data=Dat)
 
 ggplot(Dat, aes(x=SiteElevation, y=Prop_rmv, colour = Canopy)) + 
   geom_point() +
-  geom_smooth(method="glm")
+  geom_smooth(method="glm") + xlab("Site Elevation") + ylab("Proportion of Seeds Removed") 
 
 
 ### let's do some stats
@@ -49,7 +49,42 @@ summary(mod1)
 library(visreg)
 visreg(mod1, xvar="SiteElevation", by="Canopy", overlay=T)
 
-# this is appropriate but won't run
+
+############################
+
+#### scaled canopy but it is only two categories, do not know if that is allowed**? 
+
+### changing canopy into a numerical variable to scale it 
+##### closed = 1 , open = 2 
+
+Dat$CanopyNum <- ifelse(Dat$Canopy == 'closed', 2, 1)
+
+###install.packages("lme4")
+library(lme4)
+
+#Try scaling predictor variables
+####install.packages("standardize")
+library(standardize)
+Dat$scaledsitelev <- scale(Dat$SiteElevation)
+###Dat$scaledCanopy <- scale(Dat$CanopyNum)
+
+
+mod2 <- glmer(Prop_rmv ~ scaledsitelev*Canopy + (1|Species) + (1|Date2) + (1|Site), family="binomial", control= glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun=100000)), data=Dat)
+
+visreg(mod2, xvar="scaledsitelev", by="Canopy", overlay=T, scale ="response")
+summary(mod2)
+
+install.packages("ggeffects")
+library(ggeffects)
+
+ggtest <- ggpredict(mod2,terms=c("scaledsitelev","Canopy"))
+plot(ggtest, rawdata = TRUE )
+
+
+############################
+
+
+# this is appropriate but won't run (back up cause i'm playing around to try and get mod2 working)
 mod2 <- glmer(Prop_rmv ~ SiteElevation*Canopy + (1|Species) + (1|Date2) + (1|Site), family="binomial", control= glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun=100000)), data=Dat)
 
 # this has proper handling of bounded response variable but has pseudoreplication by dropping random effect of Site and doesn't adjust for differences between species or sampling dates
@@ -66,17 +101,6 @@ Species <- Dat$Species
 ggplot(Dat, aes(x=SiteElevation, y=Prop_rmv, colour = Species)) + 
   geom_point() +
   geom_smooth(method="glm") + theme_minimal() 
-
-
-### 
-
-
-
-
-
-
-
-
 
 
 
